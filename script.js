1e1d412317c4ffcd796fe9309a56209c4cd0f64a
Benjamin Carayon <benjamin.carayon2025@campus-eni.fr>
2025-04-14 12:45:47 +0200

    Reusable Nav and different pages

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 window.onload = init;
 function init() {
-    document.querySelectorAll('nav ul li').forEach(li => li.addEventListener('click', loadPage));
-    document.getElementById('nav-accueil').click();
-    document.querySelector('nav button').addEventListener('click', toggleMenu);
+    loadNav();
 }
 
-function loadPage(event) {
-    fetch(`./${event.currentTarget.id.split('nav-').pop()}.htm`)
+function loadNav() {
+    fetch(`./nav.htm`)
     .then(response => response.text())
     .then(data => {
-        let li = event.target.tagName == 'A' ? event.target.parentElement : event.target;
-        document.querySelector('main').innerHTML = data;
-        document.title = "PromoENI - " + document.querySelector('h2').innerText.split(' ').shift();
+        const nav = document.querySelector('nav');
+        nav.innerHTML = data;
+        const li = nav.querySelector('#nav-' + window.location.pathname.split('/').pop().split('.htm')[0]);
+
+        nav.querySelector('nav button').addEventListener('click', toggleMenu)
+
         document.querySelectorAll('nav ul li').forEach(li => li.classList.remove("nav-active"));
         li.classList.add("nav-active");
         if (window.innerWidth < 800) toggleMenu();
