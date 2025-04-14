351767e901eb24f9749d205dc126af44e9a9e142
Benjamin Carayon <benjamin.carayon2025@campus-eni.fr>
2025-04-14 16:39:16 +0200

    Working preferences page

--- script.js
+++ script.js
@@ -1,7 +1,142 @@
+const pageName = window.location.pathname.split('/').pop().split('.htm')[0];
+
 window.onload = init;
 function init() {
     loadNav();
+    const radios = document.querySelectorAll('[role="radiogroup"]');
+    for (let i = 0; i < radios.length; i++) {
+      new RadioGroup(radios[i]);
+    }
+
+    const themeValue = localStorage.getItem('theme');
+    const displayValue = localStorage.getItem('display');
+    if (themeValue) {
+        const option = document.querySelector(`option[value=${themeValue}]`);
+        if (option) option.setAttribute("selected", true);
+        document.body.setAttribute('data-theme', themeValue)
+    }
+    if (displayValue) {
+        const radio = document.querySelector(`[role='radio'][value=${displayValue}]`);
+        if (radio) radio.click();
+    }
 }
+
+class RadioGroup {
+    constructor(groupNode) {
+      this.groupNode = groupNode;
+  
+      this.radioButtons = [];
+  
+      this.firstRadioButton = null;
+      this.lastRadioButton = null;
+  
+      var rbs = this.groupNode.querySelectorAll('[role=radio]');
+  
+      for (var i = 0; i < rbs.length; i++) {
+        var rb = rbs[i];
+  
+        rb.tabIndex = -1;
+        rb.setAttribute('aria-checked', 'false');
+  
+        rb.addEventListener('keydown', this.handleKeydown.bind(this));
+        rb.addEventListener('click', this.handleClick.bind(this));
+        rb.addEventListener('focus', this.handleFocus.bind(this));
+        rb.addEventListener('blur', this.handleBlur.bind(this));
+  
+        this.radioButtons.push(rb);
+  
+        if (!this.firstRadioButton) {
+          this.firstRadioButton = rb;
+        }
+        this.lastRadioButton = rb;
+      }
+      this.firstRadioButton.tabIndex = 0;
+    }
+  
+    setChecked(currentItem) {
+      for (var i = 0; i < this.radioButtons.length; i++) {
+        var rb = this.radioButtons[i];
+        rb.setAttribute('aria-checked', 'false');
+        rb.tabIndex = -1;
+      }
+      currentItem.setAttribute('aria-checked', 'true');
+      currentItem.tabIndex = 0;
+      currentItem.focus();
+    }
+  
+    setCheckedToPreviousItem(currentItem) {
+      var index;
+  
+      if (currentItem === this.firstRadioButton) {
+        this.setChecked(this.lastRadioButton);
+      } else {
+        index = this.radioButtons.indexOf(currentItem);
+        this.setChecked(this.radioButtons[index - 1]);
+      }
+    }
+  
+    setCheckedToNextItem(currentItem) {
+      var index;
+  
+      if (currentItem === this.lastRadioButton) {
+        this.setChecked(this.firstRadioButton);
+      } else {
+        index = this.radioButtons.indexOf(currentItem);
+        this.setChecked(this.radioButtons[index + 1]);
+      }
+    }
+  
+    /* EVENT HANDLERS */
+  
+    handleKeydown(event) {
+      var tgt = event.currentTarget,
+        flag = false;
+  
+      switch (event.key) {
+        case ' ':
+          this.setChecked(tgt);
+          flag = true;
+          break;
+  
+        case 'Up':
+        case 'ArrowUp':
+        case 'Left':
+        case 'ArrowLeft':
+          this.setCheckedToPreviousItem(tgt);
+          flag = true;
+          break;
+  
+        case 'Down':
+        case 'ArrowDown':
+        case 'Right':
+        case 'ArrowRight':
+          this.setCheckedToNextItem(tgt);
+          flag = true;
+          break;
+  
+        default:
+          break;
+      }
+  
+      if (flag) {
+        event.stopPropagation();
+        event.preventDefault();
+      }
+    }
+  
+    handleClick(event) {
+      this.setChecked(event.currentTarget);
+    }
+  
+    handleFocus(event) {
+      event.currentTarget.classList.add('focus');
+    }
+  
+    handleBlur(event) {
+      event.currentTarget.classList.remove('focus');
+    }
+  }
+
 
 function loadNav() {
     fetch(`./nav.htm`)
@@ -9,7 +144,7 @@ function loadNav() {
     .then(data => {
         const nav = document.querySelector('nav');
         nav.innerHTML = data;
-        const li = nav.querySelector('#nav-' + window.location.pathname.split('/').pop().split('.htm')[0]);
+        const li = nav.querySelector('#nav-' + pageName);
 
         nav.querySelector('nav button').addEventListener('click', toggleMenu)
 
@@ -31,4 +166,19 @@ function toggleMenu() {
         span.innerText = 'menu';
         ul.style.display = 'none';
     }
+}
+function saveDisplay() {
+    const value = document.querySelector("[aria-checked='true']").getAttribute('value');
+    localStorage.setItem('display', value);
+}
+
+function saveTheme() {
+    const value =  document.querySelector('select').value;
+    localStorage.setItem('theme', value);
+    document.body.setAttribute('data-theme', value);
+}
+
+function savePreferences() {
+    saveDisplay();
+    saveTheme();
 }
